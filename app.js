354fcd9259390197f222ca3f534f71bd6c0f638a
mh4fwkftjc <146381415+mh4fwkftjc@users.noreply.github.com>
2024-04-24 17:37:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textcolor = :"#FFFFFFF"
+tg.MainButton.color = "#2cab37";
+
+let item ="";
+
+let btn1 = "btn1";
+let btn2 = "btn2";
+let btn3 = "btn3";
+let btn4 = "btn4";
+let btn5 = "btn5";
+let btn6 = "btn6";
+
+btn1.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+btn2.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "2";
+        tg.MainButton.show();
+    }
+})
+btn3.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "3";
+        tg.MainButton.show();
+    }
+})
+btn4.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "4";
+        tg.MainButton.show();
+    }
+})
+btn5.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "5";
+        tg.MainButton.show();
+    }
+})
+btn6.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        item = "6";
+        tg.MainButton.show();
+    }
+})
+
+Telegram.WebApp.onEvent("mainButtonClicked",function(){
+    tg.sendData(item);
+});
+
+let usercard = document.getElementById("usercard")
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.first_name}
+${tg.initDataUnsafe.first_name}`;
+
+usercard.appendChild(p);
+
+
+
