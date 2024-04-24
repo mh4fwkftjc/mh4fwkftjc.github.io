5a247e621b987e153a278c4e4a8348f5a82dde12
mh4fwkftjc <146381415+mh4fwkftjc@users.noreply.github.com>
2024-04-24 18:32:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ let usercard = document.getElementById("usercard")
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.first_name}
-${tg.initDataUnsafe.first_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
 
